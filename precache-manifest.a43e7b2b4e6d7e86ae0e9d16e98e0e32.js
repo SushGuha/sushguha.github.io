2380ea323137e829945583fda8925703b5a49e95
Sushant Guha <s.guha@mail.utoronto.ca>
2021-11-18 01:05:28 -0500

    first build

---
+++ precache-manifest.a43e7b2b4e6d7e86ae0e9d16e98e0e32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bfc9f64ea2f6ded5c0234200d4fe6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3449086034aa7b27d74",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "e8090cf7e105881ec703",
+    "url": "/static/css/main.ef14ebbe.chunk.css"
+  },
+  {
+    "revision": "c3449086034aa7b27d74",
+    "url": "/static/js/2.36300f8c.chunk.js"
+  },
+  {
+    "revision": "f445187dbc1eb62faaf89821ad3a61da",
+    "url": "/static/js/2.36300f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef66ff0451a6c972ad7",
+    "url": "/static/js/3.3aab005b.chunk.js"
+  },
+  {
+    "revision": "e8090cf7e105881ec703",
+    "url": "/static/js/main.3e0bb71b.chunk.js"
+  },
+  {
+    "revision": "f031a9b934ce1a7acbbe",
+    "url": "/static/js/runtime-main.1b6047db.js"
+  }
+]);
