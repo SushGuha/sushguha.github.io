b66f324d39ed9abd17b242f7b8fd8d06facd9e46
Sushant Guha <s.guha@mail.utoronto.ca>
2021-11-28 16:09:25 -0500

    28 nov after building

---
+++ precache-manifest.30e94fec88281cf7fb969ce65ca3ee7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad236dcbeffb991f1c1b2a946e1442b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b753155aca816e3c02cf",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "afa4bca5ca88c7ac0570",
+    "url": "/static/css/main.0a8554c6.chunk.css"
+  },
+  {
+    "revision": "b753155aca816e3c02cf",
+    "url": "/static/js/2.3923f4c4.chunk.js"
+  },
+  {
+    "revision": "f445187dbc1eb62faaf89821ad3a61da",
+    "url": "/static/js/2.3923f4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a79c553ff8c988bfd88",
+    "url": "/static/js/3.4579cfc2.chunk.js"
+  },
+  {
+    "revision": "afa4bca5ca88c7ac0570",
+    "url": "/static/js/main.4a9a3672.chunk.js"
+  },
+  {
+    "revision": "f603be9aa6240bc9972b",
+    "url": "/static/js/runtime-main.9f3ab35e.js"
+  },
+  {
+    "revision": "f84de21441aa6b3b9c17c66d5d65d911",
+    "url": "/static/media/cropped.f84de214.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be8f87778574b6b5d5cf8dcba3e622ca.js"
+  "/precache-manifest.30e94fec88281cf7fb969ce65ca3ee7f.js"
 );
 
 self.addEventListener('message', (event) => {
